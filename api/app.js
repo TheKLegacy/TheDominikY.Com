db6139428dafe27c3990344e772f41f388aa5947
TheKlegacy <domininkkingdiepie@gmail.com>
2023-09-29 15:50:05 -0500

    update move footer to server

--- api/app.js
+++ api/app.js
@@ -20,7 +20,25 @@ app.get('/v1/htmx/nav', (req, res) => {
       // Send the HTML content as the response
       res.send(data);
     });
-  });
+});
+
+app.get('/v1/htmx/footer', (req, res) => {
+    res.setHeader('Cache-Control', 'no-cache, no-store, must-revalidate');
+    res.setHeader('Pragma', 'no-cache');
+    res.setHeader('Expires', '0');
+
+    // Read the HTML file asynchronously
+    fs.readFile('components/footer.html', 'utf8', (err, data) => {
+        if (err) {
+        console.error('Error reading HTML file:', err);
+        res.status(500).send('Internal Server Error');
+        return;
+        }
+        
+        // Send the HTML content as the response
+        res.send(data);
+    });
+}); 
   
 
 // Start the server
