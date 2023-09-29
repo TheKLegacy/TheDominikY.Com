879311c636b40e07890440e1e33445c28c04d68b
TheKlegacy <domininkkingdiepie@gmail.com>
2023-09-29 16:13:10 -0500

    update nav

--- api/app.js
+++ api/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const fs = require('fs');
+const cheerio = require('cheerio'); // Import the cheerio library
 const port = 3000;
 
 // Define a route that returns a simple JSON response
@@ -9,6 +10,10 @@ app.get('/v1/htmx/nav', (req, res) => {
     res.setHeader('Pragma', 'no-cache');
     res.setHeader('Expires', '0');
 
+    const activeItem = req.query.activeItem;
+
+    console.log(activeItem);
+
     // Read the HTML file asynchronously
     fs.readFile('components/nav.html', 'utf8', (err, data) => {
       if (err) {
@@ -16,9 +21,33 @@ app.get('/v1/htmx/nav', (req, res) => {
         res.status(500).send('Internal Server Error');
         return;
       }
-      
-      // Send the HTML content as the response
-      res.send(data);
+
+    // Load the HTML content into a cheerio object
+    const $ = cheerio.load(data);
+
+    // Find the element based on its inner HTML (e.g., "Content goes here")
+    const targetElement = $('a').filter(function() {
+        return $(this).text().toLowerCase() === activeItem.toLowerCase();
+    });
+
+    console.log(`a:contains("${activeItem}")`)
+    console.log(targetElement.html());
+
+    // Modify the class attribute of the found element
+    // Get the current class attribute value
+    let currentClass = targetElement.attr('class') || '';
+
+    // Append a new class name to the existing class (e.g., "new-class")
+    currentClass += ' active';
+
+    // Update the class attribute of the element with the new value
+    targetElement.attr('class', currentClass);
+
+    // Get the updated HTML content
+    const modifiedData = $.html();
+
+    // Send the HTML content as the response
+    res.send(modifiedData);
     });
 });
 
