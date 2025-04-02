6ebbb37093a6625a2dbb34670cd035dcc3beceb6
TheKlegacy <domininkkingdiepie@gmail.com>
2025-04-01 21:44:24 -0500

    fix projects page an update stale content

--- app.js
+++ app.js
@@ -1,52 +1,43 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const Html = require('./html');
-const Blog = require('./blog');
+const Html = require("./html");
+const Blog = require("./blog");
+const Projects = require("./projects");
 const port = 3000;
 
 app.use((req, res, next) => {
-    res.setHeader('Cache-Control', 'no-cache, no-store, must-revalidate');
-    res.setHeader('Pragma', 'no-cache');
-    res.setHeader('Expires', '0');
+    res.setHeader("Cache-Control", "no-cache, no-store, must-revalidate");
+    res.setHeader("Pragma", "no-cache");
+    res.setHeader("Expires", "0");
     next();
-}); 
-
-app.get('/v1/htmx/footer', (req, res) => {
-    Html.sendHtmlFile('components/footer.html', res);
 });
 
-app.get('/', (req, res) => {
-    Html.constructStandardPage("home", res);
+app.get("/v1/htmx/footer", (req, res) => Html.sendHtmlFile("components/footer.html", res));
+
+app.get("/", (req, res) => Html.constructStandardPage("home", res));
+
+app.get("/projects", (req, res) => Html.constructStandardPage("projects", res));
+
+app.get("/projects/content", (req, res) => {
+    const { filter, sort, direction } = req.query;
+    res.send(Projects.displayProjects(filter, sort, direction));
 });
 
-app.get('/projects', (req, res) => {
-    Html.constructStandardPage("projects", res);
+app.get("/about", (req, res) => Html.constructStandardPage("about", res));
+
+app.get("/resume", (req, res) => Html.constructStandardPage("resume", res));
+
+app.get("/blog", (req, res) => Html.constructStandardPage("blog", res));
+
+app.get("/v1/htmx/blog/items", async (req, res) => {
+    const { filter, sort } = req.query;
+    res.send(await Blog.getCards(filter, sort));
 });
 
-app.get('/about', (req, res) => {
-    Html.constructStandardPage("about", res);
-});
+app.get("/v1/htmx/blog/entry", async (req, res) =>
+    Html.sendHtmlFile(`components/blog/entries/${req.query.id}.html`, res)
+);
 
-app.get('/resume', (req, res) => {
-    Html.constructStandardPage("resume", res);
-});
-
-app.get('/blog', (req, res) => {
-    Html.constructStandardPage("blog", res);
-});
-
-app.get('/v1/htmx/blog/items', async (req, res) => {
-    const filter = req.query.filter;
-    const sort = req.query.sort;
-    res.send(await Blog.getCards(filter, sort))
-});
-
-app.get('/v1/htmx/blog/entry', async (req, res) => {
-    const id = req.query.id;
-    Html.sendHtmlFile(`components/blog/entries/${id}.html`,res);
-});
-
-
-app.listen(port, () => {
-    console.log(`Server is running on http://localhost:${port}`);
-});
+app.listen(port, () =>
+    console.log(`Server is running on http://localhost:${port}`)
+);
---
+++ projects.js
@@ -0,0 +1,156 @@
+const projects = [
+    {
+        id: "build-a-metagame",
+        name: "Build A Metagame",
+        createDate: "2015",
+        stopDate: "2018",
+        lang: ["HTML5", "CSS3", "JavaScript"],
+        image: "assets/imgs/bamhub.jpg",
+        description:
+            "Build a metagame (BAM) is a Pokémon fakedex focused on creating a new competitive Pokémon metagame. We have our own forum and Pokédex and once was planned to be functioning on Pokémon Showdown. [THIS IS NOW DEFUNCT] To view the archived version",
+        link: "/bam/index4.html",
+        linkText: "click here",
+    },
+    {
+        id: "html5-roguelike",
+        name: "HTML5 Roguelike",
+        createDate: "2013",
+        stopDate: "2018",
+        lang: ["HTML5", "CSS3", "JavaScript"],
+        image: "assets/imgs/rouguepreview.png",
+        description:
+            "This is an HTML roguelike RPG that I made completely from scratch with free graphics.",
+    },
+    {
+        id: "java-snake-game",
+        name: "Java Snake Game",
+        createDate: "2016",
+        stopDate: "2016",
+        lang: ["Java", "Eclipse"],
+        image: "assets/imgs/snakejava.jpg",
+        description:
+            "Made in AP Computer Science with David Ruth and Charles Jacob. A simple Snake game that keeps track of the winner in a two player mode (Kinda like slither.io) or classic single player. This game also implements music in Java. Use arrow keys or WASD to move, 1 and 2 to change player modes and R to restart.",
+    },
+    {
+        id: "rby2k20",
+        name: "RBY2K20",
+        createDate: "2020",
+        stopDate: "2020",
+        lang: ["JavaScript", "CSS3", "HTML5", "PHP"],
+        image: "rby2k20/images/alphalogobig.png",
+        description:
+            "RBY2k20 is a Generation 1 competitive Pokémon community. The website developed by myself contains a Pokédex and simple CMS. To see it",
+        link: "http://thedominiky.com/rby2k20/",
+        linkText: "click here",
+    },
+    {
+        id: "unity-roguelike",
+        name: "Unity Roguelike",
+        createDate: "2017",
+        stopDate: "2018",
+        lang: ["Unity", "C#"],
+        image: "assets/imgs/unityrougue.PNG",
+        description:
+            "Roguelike game made in Unity and C# as a senior project in high school with my good friend Rishi Patel. The game had a combat system, leveling, shopping, and dungeoneering.",
+    },
+    {
+        id: "space-invaders",
+        name: "Space Invaders",
+        createDate: "2016",
+        stopDate: "2016",
+        lang: ["HTML5", "JavaScript", "Phaser.js"],
+        image: "assets/imgs/spaceinvaders.jpg",
+        description: "A Space Invaders clone built using Phaser.js framework.",
+    },
+    {
+        id: "breakout",
+        name: "Breakout",
+        createDate: "2016",
+        stopDate: "2016",
+        lang: ["HTML5", "JavaScript", "Phaser.js"],
+        image: "assets/imgs/breakout.jpg",
+        description:
+            "A classic Breakout/Arkanoid style game created with Phaser.js.",
+    },
+    {
+        id: "multiplayer-ctf-paintball",
+        name: "Multi-Player CTF Paintball",
+        createDate: "2017",
+        stopDate: "2017",
+        lang: ["Unity", "C#"],
+        image: "assets/imgs/paintball.jpg",
+        description:
+            "A multiplayer Capture The Flag paintball game developed in Unity.",
+    },
+];
+
+function generateProjectHTML(project) {
+    let html = `
+        <div class="project-item container" data-id="${
+            project.id
+        }" data-name="${project.name.toLowerCase()}" data-lang="${project.lang
+        .join(" ")
+        .toLowerCase()}" data-create="${project.createDate}" data-update="${
+        project.stopDate
+    }">
+            <h3>${project.name}</h3>
+            <img class="img-thumbnail rounded" src="${project.image}" alt="${
+        project.name
+    } screenshot">
+            <p>
+                ${project.description}
+                ${
+                    project.link
+                        ? `<a href="${project.link}">${project.linkText}</a>`
+                        : ""
+                }
+            </p>
+            <span>Created: ${project.createDate}&nbsp;&nbsp;&nbsp; Updated: ${
+        project.stopDate
+    }</span>
+            <div class="tech-tags">
+                ${project.lang
+                    .map(
+                        (tech) =>
+                            `<span class="badge badge-primary">${tech}</span>`
+                    )
+                    .join(" ")}
+            </div>
+        </div>
+        <br>
+    `;
+    return html;
+}
+
+function displayProjects(searchFilter, sort, direction) {
+    let filteredProjects = [...projects]
+
+    if (searchFilter) {
+        filteredProjects = projects.filter((proj) =>
+            [proj.name, proj.description].some((field) =>
+                field.toLowerCase().includes(searchFilter.toLowerCase())
+            )
+        );
+    }
+
+    const sortKey =
+        sort === "alpha"
+            ? "name"
+            : sort === "date"
+            ? "stopDate"
+            : sort === "create"
+            ? "createDate"
+            : null;
+
+    if (sortKey) {
+        filteredProjects.sort((a, b) =>
+            direction === "asc"
+                ? a[sortKey].localeCompare(b[sortKey])
+                : b[sortKey].localeCompare(a[sortKey])
+        );
+    }
+
+    return filteredProjects.map(generateProjectHTML).join("");
+}
+
+module.exports = { displayProjects };
